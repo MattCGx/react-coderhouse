a2cb4983711ad2c3afae4645a6ff878120d6b7fe
MattCG <mattcontrerasg@gmail.com>
2024-02-21 01:16:34 -0300

    init2

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+
+
+const CartWidget = (props) => {
+    return (
+        <img src={props.cartimg} />
+    )
+}
+
+export default CartWidget
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import classes from './Navbar.modules.css'
+import classes from "./Navbar.modules.css";
+import CartWidget from "../CartWidget/CartWidget";
+import Button from "../Button/Button";
 
 const Navbar = () => {
   return (
     <header className={classes.header}>
-  <h4>NavBar</h4>
-  <ul>
-<li><a href="">televisores</a></li>
-<li><a href="">celulares</a></li>
-<li><a href="">pc</a></li>
-<li><a href="">audio</a></li>
-
-  </ul>
+      <i></i>
+      <ul>
+        <li>
+          <a href="">televisores</a>
+        </li>
+        <li>
+          <a href="">celulares</a>
+        </li>
+        <li>
+          <a href="">pc</a>
+        </li>
+        <li>
+          <a href="">audio</a>
+        </li>
+      </ul>
+      <CartWidget />
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
